1a37a7d52109f2e2c003ac4d14dd28259d778b94
Justin Le <lej42@uci.edu>
2024-01-22 18:34:18 -0800

    updated a1p1.py and added lab4.py

--- ICS_32/Assignments/a1p1.py
+++ ICS_32/Assignments/a1p1.py
@@ -1,45 +1,96 @@
 from pathlib import Path
-'''
-myPath = Path(file_input)
-        if myPath.exists():
-            for folders in myPath.iterdir():
-                print(folders)
 
 #THIS IS -R
-def recursive():
+def recursive(path):
+    for paths in path.iterdir():
+        if paths.is_dir():
+            print(paths)
+            recursive(paths)
+        else:
+            print(paths)
+
 #THIS IS -F
-def only_files():
-    for paths in myPath.iterdir():
-            if paths.isfile():
-                 print(paths)
-            
+def only_files(path):
+    for paths in path.iterdir():
+        if paths.is_file():
+            print(paths)
+  
 #THIS IS -S
-def only_same_name():
+def only_same_name(path, keyword, path_n):
+    path_name = path_n.split()
+    for paths in path.iterdir():
+        if path_name[-1] == keyword:
+            print(paths)
+
 #THIS IS -E
-def only_same_extension():
-'''
+def only_same_extension(path, keyword, path_n):
+    path_name = path_n.split()
+    for paths in path.iterdir():
+        if keyword in path_name[-1]:
+            print(paths)
+
+#This is R and F
+def recursive_files(path):
+    for paths in path.iterdir():
+        if paths.is_dir():
+            recursive(paths)
+        else:
+            print(paths)
 def content_list():
     for paths in myPath.iterdir():
         print(paths)
 
 
 if __name__ == "__main__":
-    user_input = "L /Users/justinle/VS_Code/ICS_32"
-
+    user_input = "L /Users/justinle/VS_Code -r"
     while user_input != "Q":
         user_input_list = user_input.split()
         command = user_input_list[0]
-        path = user_input_list[1]
-        myPath = Path(path)
-        if command != "L":
-            print("Command can only be Q or L, Please choose again!")
-            user_input = input()
-        else:
+        try:
+            path = user_input_list[1]
+            myPath = Path(path)
+        except:
+            path = ""
+        if command == "L":
             if myPath.exists():
                 if len(user_input_list) == 2:
                     content_list()
                     user_input = input()
+                elif len(user_input_list) == 3:
+                    option = user_input_list[2]
+                    if option == "-r":
+                        recursive(myPath)
+                        user_input = input()
+                    elif option == "-f":
+                        only_files(myPath)
+                        user_input = input()
+                    elif option == "-e":
+                        print("Need to specify file name, try again!")
+                        user_input = input()
+                    elif option == "-s":
+                        print("Need to specify file extension, try again!")
+                        user_input = input()
+                    else: 
+                        print("Invalid Option must be -r, -f, -e, or -s, Try again!")
+                        user_input = input()
+            
+                elif len(user_input_list) == 4:
+                    option = user_input_list[2]
+                    other_input = user_input_list[3]
+                    if option == "-e":
+                        only_same_extension(myPath, other_input, path)
+                        user_input = input()
+                    elif option == "-f":
+                        only_same_extension(myPath, other_input, path)
+                        user_input = input()
+                    elif option == "-r" and other_input == "-f":
+                        recursive_files(myPath)
+                        user_input = input()
+                    
+                    
             else:
                  print("file or folder doesn't exist, Please choose again!")
                  user_input = input()
-            
+        elif command != "L" or command != "Q":
+            print("command must be L or Q, try again")
+            user_input = input()
